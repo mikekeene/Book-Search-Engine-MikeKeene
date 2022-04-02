5aaff83d4743dbe47354988382e6abc414b8571b
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-02 15:16:46 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/booksearch', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  //needed for mongoose 5
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
