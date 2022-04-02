109b97c1a133b6ae3a3661f13841df1ad11323cc
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-01 23:27:37 -0500

    Update queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,24 @@
 // TODO: queries.js: This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+
+//import graphQL
+import gql from 'graphql-tag';
+
+//query GET_ME (query parameters from typeDefs.js):
+export const GET_ME = gql` 
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
