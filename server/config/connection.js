79d7a4ca86f5ed0c0e4410e11fc5b7021f169515
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-01 23:27:04 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
