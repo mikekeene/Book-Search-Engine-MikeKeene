1b1a31727aa41d2f34e5a16472c8f74cb3f3b67d
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-01 22:16:49 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 //TODO: Implement Apollo Server & apply it to Express server as middleware
 const express = require('express');
+// implement apollo server:
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// import schemas
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const mikeServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
