90b6bacf9e074a21a7e07e9d76d5dcfd98339747
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-03 12:11:55 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,33 @@
 const express = require('express');
+// path required for deploying static files
 const path = require('path');
+// importing in mongoose/mongodb connection
 const db = require('./config/connection');
+//import dotenv for .env use
 require('dotenv').config();
 const {ApolloServer} = require('apollo-server-express');
 
-
+// import schema (typeDefs + resolvers)
 const {typeDefs, resolvers} = require('./schemas');
+// import authentication functionality 
 const {authMiddleware} = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+// creating new Apollo server and passing in schema and authenication middleware
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
+// combing apollo server with express 
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// serve up the files in "build" folder when in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
@@ -30,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-
+//once database connection is active, connect to Express/API server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
