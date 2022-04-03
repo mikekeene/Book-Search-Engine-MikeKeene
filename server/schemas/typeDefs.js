ede03b215988ce17ebfd60b57425cdd54184043d
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-04-03 01:31:54 -0500

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,19 +4,9 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     # Book type: bookId, authors, description, title, image, link, 
     type Book {
+        bookId: String
         authors: [String]
         description: String
-        bookId: String
-        image: String
-        link: String
-        title: String
-    }
-
-    # saveBookInput for easier mutation readability 
-    input saveBookInput {
-        authors: [String]
-        description: String
-        bookId: String
         image: String
         link: String
         title: String
@@ -27,13 +17,13 @@ const typeDefs = gql`
         _id: ID
         username: String
         email: String
-        password: String
-        bookCount: Int
         savedBooks: [Book]
     }
     # Query type: me - which returns a User type
     type Query {
         me: User
+        users: [User]
+        user(_id: String, username: String): User
     }
     # Auth type: token, user (references the User type)
     type Auth {
@@ -49,7 +39,7 @@ const typeDefs = gql`
         # Accepts a username, email, & password as parameters; returns an Auth type
         addUser(username: String!, email: String!, password: String!): Auth
         # saveBook: returns a User type.
-        saveBook(body: saveBookInput): User
+        saveBook(bookId: String!, authors: [String]!, description: String, image: String, link: String, title: String!): User
         # removeBook: Accepts a book's bookId as a parameter; returns a User type.
         removeBook(bookId: String!): User
     }
